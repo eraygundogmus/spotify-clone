7f7827a0fcb3f30fca894a1a73094304d03af653
eraygundogmus <gundogmuseray@gmail.com>
2021-02-20 12:56:08 +0300

    sidebar css update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
 import { useDataLayerValue } from "./DataLayer";
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 function Sidebar() {
     const [{ playlists }, dispatch ] = useDataLayerValue()
@@ -12,8 +17,13 @@ function Sidebar() {
                 src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
                 alt="Logo"
                 /> 
-            <SidebarOption title="Home"/>
-
+            <SidebarOption  Icon={HomeIcon}  title="Home"/>
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+            <strong className="playlist__title"> PLAYLISTS </strong>
+            <SidebarOption Icon={LibraryAddIcon} title="Create Playlist"/>
+            <SidebarOption Icon={FavoriteIcon} title="Tracks you liked"/>
+            <hr />
             {playlists?.items?.map((playlist) => (
             <SidebarOption title={playlist.name}/> 
             
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./SidebarOption.css";
 
-function SidebarOption( { title }) {
+function SidebarOption( { title, Icon }) {
     return (
         <div className="sidebarOption">
-            <p>{title}</p>
-            
+            {Icon && <Icon className="sidebarOption__icon" />}
+            {Icon ? <h4>{title} </h4> : <p>{title}</p> }
         </div>
     )
 }
